d6bb01dde79049d992a344bb8b9d38f8ece8aa43
AudriusAnusauskas <aaaudrius@gmail.com>
2023-05-28 14:17:52 +0300

    finish module 4.1 stage0

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -2,6 +2,36 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class DaysInMonth {
     public void printDays(int year, int month) {
+        boolean leapYear;
+        switch (year % 4){
+            default:
+                leapYear = false;
+                break;
+            case  0:
+                if (year % 100 != 0 || year % 400 == 0) {
+                    leapYear = true;
+                } else {
+                    leapYear = false;
+                }
+                break;
+        }
 
+
+
+       String days = switch (month){
+            case 1, 3, 5, 7, 8, 10, 12 -> {
+                yield "31";
+            }
+            case 4, 6, 9, 11 -> {
+                yield "30";
+            }
+            case 2 -> {
+               yield leapYear ? "29" : "28";
+            }
+            default -> {
+                yield "invalid date";
+            }
+        };
+        System.out.println(days);
     }
 }
--- src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
@@ -2,5 +2,28 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class IntegerDivider {
     public void printCompletelyDivided(int dividend, int divider) {
+
+        switch (divider){
+            case 0:
+                System.out.println("division by zero");
+                break;
+            default:
+                int division = dividend / divider;
+                int product = division * divider;
+                if (product == dividend){
+                    System.out.println("can be divided completely");
+                } else {
+                    System.out.println("cannot be divided completely");
+                }
+                break;
+        }
+
+
     }
+    public static void main(String[] args) {
+        IntegerDivider completelyDivided = new IntegerDivider();
+
+        completelyDivided.printCompletelyDivided(25, 2);
+    }
+
 }
